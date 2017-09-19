98c43c93a97744f5fa066012d613c099b4369bf3
Smith <JDSmith@magellanhealth.com>
2017-09-19 16:32:11 -0700

    Continue Deployment module

--- deploy-demo/src/app/navbar/navbar.component.ts
+++ deploy-demo/src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
+import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'navbar',
+  selector: 'nav-bar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  backgroundColor = environment.navBarBackgroundColor;
 
   constructor() { }
 
